811aeec349423309dce352c2c792f84e75179117
esyaahmad <esya.ahmad@gmail.com>
2024-03-19 03:41:39 +0700

    screen jalan, belum wiring

---
+++ server/schemas/categories.js
@@ -0,0 +1,31 @@
+const { findALlCategories } = require("../models/categories");
+
+
+const typeDefs = `#graphql
+
+type Category {  
+    _id : ID
+    name: String
+    logo: String
+}
+
+type Query {
+    getAllCategory: [Category]
+}
+
+`;
+
+const resolvers ={
+    Query: {
+        getAllCategory: async () => {
+            const categories = await findALlCategories()
+            // console.log(categories, "ini categories");
+            return categories
+        }
+    }
+}
+
+module.exports ={
+    CategoriesTypeDefs: typeDefs,
+    categoriesResolvers: resolvers
+}
---
+++ server/schemas/coachs.js
@@ -0,0 +1,57 @@
+const { ObjectId } = require("mongodb");
+const { findAllCoachs, AddNewCoachs } = require("../models/coachs");
+
+const typeDefs = `#graphql
+
+type Coach {
+    _id : ID
+    name: String
+    sport: String
+    Users: [User]
+    locationId: ID
+    email: String
+    imgUrl: String
+}
+
+input AddNewCoach {
+    name: String
+    sport: String
+    email: String
+    locationId: ID
+}
+
+type Query {
+    getAllCoachs: [Coach]
+}
+
+type Mutation{
+    AddCoachs(payload: AddNewCoach):Coach
+}
+
+`;
+
+const resolvers = {
+  Query: {
+    getAllCoachs: async () => {
+      const coachs = await findAllCoachs();
+
+    //   console.log(coachs, "ini coachs");
+      return coachs;
+    },
+  },
+  Mutation: {
+    AddCoachs: async (_parents, args, contextValue) => {
+      const { payload } = args;
+
+      payload.locationId = new ObjectId(payload.locationId)
+
+      const posts = await AddNewCoachs(payload);
+      return posts;
+    },
+  },
+};
+
+module.exports = {
+  coachTypeDefs: typeDefs,
+  coachResolvers: resolvers,
+};
